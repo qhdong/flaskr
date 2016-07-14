3879ab5b15bf218b6a80d278b07a5b31cf2f4719
Dong Qihong <dongqihong@vip.qq.com>
2016-07-14 20:09:30 +0800

    完成功能

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
+"""
+
 
 import os
 import sqlite3
@@ -25,3 +32,73 @@ def connect_db():
     return rv
 
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initialize the database."""
+    init_db()
+    print('Initialized the databse.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet
+    for the current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS entries;
 CREATE TABLE entries (
-  id integer PRIMARY KEY AUTOINCREMENT ,
-  title text NOT NULL ,
+  id integer PRIMARY KEY AUTOINCREMENT,
+  title text NOT NULL,
   'text' text NOT NULL
 );
